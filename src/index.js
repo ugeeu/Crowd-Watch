b428b23215a94a37f62dddb63766e15a3e9e508e
Geeage Team <eugene@geeage.com>
2021-11-19 16:59:23 +0200

    [2nd Upload]

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter as Router, Switch, Route, Redirect, Link } from 'react-router-dom'
 import './base.css'
-
+import Demo1 from './demos/Demo1'
+import Demo2 from './demos/Demo2'
 
 // Demos inspired by:
 // https://twitter.com/beesandbombs/status/1329796242298245124
